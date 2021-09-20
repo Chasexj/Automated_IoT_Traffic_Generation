3fb66ae675900af5f7b91f6b2434c06da7c1a0b6
Xi Jiang <xijiang9@nool.a.cs.uchicago.edu>
2021-09-20 13:15:54 -0500

    cleaned up codes

--- Arm.py
+++ Arm.py
@@ -1,3 +1,21 @@
+# The following code is from https://github.com/Chasexj/robotic_surgery/tree/master/src/ros/modified_arm/InvKin
+  
+'''
+Copyright (C) 2013 Travis DeWolf
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
+
 import numpy as np
 import scipy.optimize
 import math
